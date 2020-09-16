7e9f16abc2c784ab5a6629b28cbd81355904edf0
stfsy <s.pfaffel@gmail.com>
2020-09-16 08:15:22 +0200

    feat: add runner methods for cli shorthands

--- test/spec/index.spec.js
+++ test/spec/index.spec.js
@@ -45,11 +45,72 @@ describe('BroccoliTestRunner', () => {
         })
     })
 
+    describe('.serve', () => {
+        let page = null
+
+        before(() => {
+            return runner.s()
+                .then(() => puppeteer.launch({ headless: true }))
+                .then((browser) => {
+                    return browser.newPage()
+                }).then((p) => {
+                    page = p
+                })
+        })
+        after(() => {
+            return runner.stop()
+                .then(() => page.browser().close())
+        })
+
+        it('serves content', () => {
+            return page.goto('http://localhost:4200/test.html')
+                .then(() => {
+                    return page.evaluate(() => {
+                        return document.querySelector("h1").textContent;
+                    })
+                }).then((text) => {
+                    expect(text).to.contain("Hello!")
+                })
+        })
+    })
+
     describe('.build', () => {
         let page = null
 
         before(() => {
             return runner.build()
+                .then(() => puppeteer.launch({ headless: true }))
+                .then((browser) => {
+                    return browser.newPage()
+                }).then((p) => {
+                    page = p
+                })
+        })
+        after(() => {
+            return runner.stop()
+                .then(() => page.browser().close())
+        })
+        it('does not serve content', () => {
+
+            return page.goto('http://localhost:4200/test.html')
+                .then((text) => {
+                    throw ('Should not arrive here. Broccoli should not serve')
+                })
+                .catch((error) => {
+                    expect(error.message).to.contain('net::ERR_CONNECTION_REFUSED')
+                })
+        })
+        it('builds content', () => {
+            const copiedContent = fs.readFileSync('test/fixtures/dist/test.html', 'utf-8')
+            expect(copiedContent).to.contain('Hello!')
+        })
+    })
+
+    describe('.b', () => {
+        let page = null
+
+        before(() => {
+            return runner.b()
                 .then(() => puppeteer.launch({ headless: true }))
                 .then((browser) => {
                     return browser.newPage()
